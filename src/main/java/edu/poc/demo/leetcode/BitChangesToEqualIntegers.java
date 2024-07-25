1e445334ccb6f165856f845290083ec0cc657a88
leo (personal account) <leonardo.andrade6@hotmail.com>
2024-07-25 11:34:55 -0300

    n bit Changes to equal ints + merge sort array

---
+++ src/main/java/edu/poc/demo/leetcode/BitChangesToEqualIntegers.java
@@ -0,0 +1,42 @@
+package edu.poc.demo.leetcode;
+
+/**
+ * 3226. Number of Bit Changes to Make Two Integers Equal
+ *
+ * <p>You are given two positive integers n and k.
+ *
+ * <p>You can choose any bit in the binary representation of n that is equal to 1 and change it to
+ * 0.
+ *
+ * <p>Return the number of changes needed to make n equal to k. If it is impossible, return -1.
+ */
+public class BitChangesToEqualIntegers {
+
+  public static void main(String[] args) {
+    BitChangesToEqualIntegers bitChangesToEqualIntegers = new BitChangesToEqualIntegers();
+    System.out.println(bitChangesToEqualIntegers.minChanges(13, 401));
+  }
+
+  public int minChanges(int n, int k) {
+
+    if (n == k) return 0;
+
+    int max = Math.max(n, k);
+
+    int counter = 0;
+
+    for (int i = 0; n > 0; i++) {
+
+      int nRem = n % 2;
+      int kRem = k % 2;
+
+      if ((nRem == 1) && (kRem == 0)) counter++;
+      else if ((nRem == 0) && (kRem == 1)) return -1;
+
+      n = n / 2;
+      k = k / 2;
+    }
+
+    return counter;
+  }
+}
---
+++ src/main/java/edu/poc/demo/leetcode/MergeSortAnArray.java
@@ -0,0 +1,58 @@
+package edu.poc.demo.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 912. Sort an Array
+ *
+ * <p>Given an array of integers nums, sort the array in ascending order and return it.
+ *
+ * <p>You must solve the problem without using any built-in functions in O(nlog(n)) time complexity
+ * and with the smallest space complexity possible.
+ */
+public class MergeSortAnArray {
+
+  public static void main(String[] args) {
+    MergeSortAnArray mergeSortAnArray = new MergeSortAnArray();
+    int[] arr = {3, 2, 1, 14, 4, 5, 7, 6, 8, 9, 0, 10, 13};
+    int[] sorted = mergeSortAnArray.mergeSort(arr);
+    for (int j : sorted) {
+      System.out.println(j);
+    }
+  }
+
+  private int[] mergeSort(int[] arr) {
+
+    if (arr.length <= 1) return arr;
+
+    int[] left = Arrays.copyOfRange(arr, 0, arr.length / 2);
+    int[] right = Arrays.copyOfRange(arr, arr.length / 2, arr.length);
+
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    return merge(left, right);
+  }
+
+  private int[] merge(int[] left, int[] right) {
+
+    int[] res = new int[left.length + right.length];
+    int i = 0, j = 0, k = 0;
+
+    while ((i < left.length) && (j < right.length)) {
+
+      if (left[i] <= right[j]) {
+        res[k++] = left[i++];
+      } else {
+        res[k++] = right[j++];
+      }
+    }
+    while (i < left.length) {
+      res[k++] = left[i++];
+    }
+    while (j < right.length) {
+      res[k++] = right[j++];
+    }
+    return res;
+  }
+}
