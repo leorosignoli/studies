7d2eed18af4d455c6f3a2e959c48fe00d2be6a07
leo <leonardo.andrade6@hotmail.com>
2024-08-15 09:36:07 -0300

    fmt

--- src/main/java/edu/poc/demo/leetcode/CombinationSum2.java
+++ src/main/java/edu/poc/demo/leetcode/CombinationSum2.java
@@ -5,44 +5,48 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * 40. Combination Sum II
- * Solved
- * Medium
+ * 40. Combination Sum II Solved Medium
  *
-
- * Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+ * <p>Given a collection of candidate numbers (candidates) and a target number (target), find all
+ * unique combinations in candidates where the candidate numbers sum to target.
  *
- * Each number in candidates may only be used once in the combination.
+ * <p>Each number in candidates may only be used once in the combination.
  *
- * Note: The solution set must not contain duplicate combinations.
+ * <p>Note: The solution set must not contain duplicate combinations.
  */
 public class CombinationSum2 {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(candidates);
-        dfs(candidates, target, 0, new ArrayList<>(), 0, res);
-        return res;
+  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    List<List<Integer>> res = new ArrayList<>();
+    Arrays.sort(candidates);
+    dfs(candidates, target, 0, new ArrayList<>(), 0, res);
+    return res;
+  }
+
+  private void dfs(
+      int[] candidates,
+      int target,
+      int start,
+      List<Integer> cur,
+      int total,
+      List<List<Integer>> res) {
+    if (total == target) {
+      res.add(new ArrayList<>(cur));
+      return;
     }
 
-    private void dfs(int[] candidates, int target, int start, List<Integer> cur, int total, List<List<Integer>> res) {
-        if (total == target) {
-            res.add(new ArrayList<>(cur));
-            return;
-        }
-
-        if (total > target || start == candidates.length) {
-            return;
-        }
-
-        // include candidates[start]
-        cur.add(candidates[start]);
-        dfs(candidates, target, start + 1, cur, total + candidates[start], res);
-        cur.remove(cur.size() - 1);
-
-        // skip candidates[start]
-        while (start + 1 < candidates.length && candidates[start] == candidates[start + 1]) {
-            start++;
-        }
-        dfs(candidates, target, start + 1, cur, total, res);
+    if (total > target || start == candidates.length) {
+      return;
     }
+
+    // include candidates[start]
+    cur.add(candidates[start]);
+    dfs(candidates, target, start + 1, cur, total + candidates[start], res);
+    cur.remove(cur.size() - 1);
+
+    // skip candidates[start]
+    while (start + 1 < candidates.length && candidates[start] == candidates[start + 1]) {
+      start++;
+    }
+    dfs(candidates, target, start + 1, cur, total, res);
+  }
 }
--- src/main/java/edu/poc/demo/leetcode/IsSubSequence.java
+++ src/main/java/edu/poc/demo/leetcode/IsSubSequence.java
@@ -1,53 +1,45 @@
 package edu.poc.demo.leetcode;
 
 /**
- * 392. Is Subsequence
- * Solved
- * Easy
+ * 392. Is Subsequence Solved Easy
  *
- * Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+ * <p>Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
  *
- * A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+ * <p>A subsequence of a string is a new string that is formed from the original string by deleting
+ * some (can be none) of the characters without disturbing the relative positions of the remaining
+ * characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
  *
+ * <p>Example 1:
  *
+ * <p>Input: s = "abc", t = "ahbgdc" Output: true Example 2:
  *
- * Example 1:
+ * <p>Input: s = "axc", t = "ahbgdc" Output: false
  *
- * Input: s = "abc", t = "ahbgdc"
- * Output: true
- * Example 2:
+ * <p>Constraints:
  *
- * Input: s = "axc", t = "ahbgdc"
- * Output: false
+ * <p>0 <= s.length <= 100 0 <= t.length <= 104 s and t consist only of lowercase English letters.
  *
- *
- * Constraints:
- *
- * 0 <= s.length <= 100
- * 0 <= t.length <= 104
- * s and t consist only of lowercase English letters.
- *
- *
- * Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+ * <p>Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you
+ * want to check one by one to see if t has its subsequence. In this scenario, how would you change
+ * your code?
  */
 public class IsSubSequence {
-    private boolean isSubsequence(String s, String t) {
+  private boolean isSubsequence(String s, String t) {
 
-        if(s.isEmpty()){
-            return true;
-        }
-        if  (t.isEmpty()){
-            return false;
-        }
-
-        int sPointer = 0;
-        for(int i =0; i < t.length(); i++){
-            if(sPointer < s.length() && t.charAt(i) == s.charAt(sPointer) ){
-                sPointer++;
-            }
-        }
-        System.out.println(sPointer);
-        return sPointer >= s.length();
-
+    if (s.isEmpty()) {
+      return true;
     }
+    if (t.isEmpty()) {
+      return false;
+    }
+
+    int sPointer = 0;
+    for (int i = 0; i < t.length(); i++) {
+      if (sPointer < s.length() && t.charAt(i) == s.charAt(sPointer)) {
+        sPointer++;
+      }
+    }
+    System.out.println(sPointer);
+    return sPointer >= s.length();
+  }
 }
